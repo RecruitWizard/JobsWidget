dffef6b526876eb288e361671b219d3b89946a44
enrique-manuel-antonio <58669286+enrique-manuel-antonio@users.noreply.github.com>
2022-01-04 12:42:41 +0000

    Replaced old angular app with new react app (#23)
    Co-authored-by: Enrique Jimenez <enrique@recruitwizard.com>
    Co-authored-by: Ben Macpherson <ben@recruitwizard.com>

---
+++ src/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import JobsWidget from './components/JobsWidget';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <JobsWidget></JobsWidget>
+    </div>
+  );
+}
+
+export default App;
